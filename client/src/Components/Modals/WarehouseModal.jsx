306fcb4c4eb103177c913ce02e5dbb0d8c72b50c
sean35mm <sean35mm@gmail.com>
2021-04-21 01:53:03 -0700

    Fixed header nav to be dynamic. Will still have to edit the path most likely. Added basic layout for both modals. Still need to do styling.

---
+++ client/src/Components/Modals/WarehouseModal.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import "./Modal.scss";
+import Modal from 'react-modal';
+import XButton from '../../Assets/Icons/close-24.svg'
+
+
+export default class WarehouseModal extends React.Component {
+  //following online guide :)
+
+  state = {
+    show: false
+  }
+  
+  //create functions for open/close state of the modal
+
+  openModal() {
+    this.setState({
+      show: true
+    })
+  }
+
+  closeModal() {
+    this.setState({
+      show: false
+    })
+  }
+
+//need to make some of the elements on the page dynamic still
+  render() {
+    return(
+      <>
+        <button className="delete_button" onClick={() => this.openModal()}></button>
+        <Modal show={this.state.show}>
+          <div className="del_modal">
+            <h1 className="modal__title">Delete this warehouse?</h1>
+            <p className="modal__body">Please confirm that you'd like to delete this from the list of warehouses. You won't be able to undo this action.</p>
+            <button className="modal__cancel button" onClick={() => this.closeModal()}>Cancel</button>
+            {/* <button>Delete</button>  I need a clickhandler to delete */}
+            <a href="" onClick={()=> this.closeModal()}><img className="modal__x-icon" src={XButton} alt="x button"/></a>
+          </div>
+        </Modal>
+      </>
+    )
+  }
