e27ade8a8f5e8b88c67b6386a7d5724907215b59
steveeum <steveeum@gmail.com>
2021-04-22 04:53:07 -0400

    intialized branch. Refactored WarehouseDetails component to class component. Partially set up backend also.

--- server/routes/warehouses.js
+++ server/routes/warehouses.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const uuid = require("uuid");
 const fs = require('fs');
 
+let warehouseList = [];
+fs.readFile('./data/warehouses.json','utf8', (err, data) => {
+    err?console.info(err):warehouseList = JSON.parse(data)});
 
+router.get('/', (req, res) => {
+    res.status(200).json(warehouseList);
+})
 
 module.exports = router;
