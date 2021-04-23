51631babb1883ac4796197e9f2a210d1abfd5de9
ajit-mathew-joseph <65310049+ajit-mathew-joseph@users.noreply.github.com>
2021-04-23 14:23:11 -0700

    Created POST request for Inventory. Added empty string validation and validation for quantities (negative).

--- server/routes/inventories.js
+++ server/routes/inventories.js
@@ -4,8 +4,9 @@ const uuid = require("uuid");
 const fs = require("fs");
 
 let inventoryList = [];
-fs.readFile('./data/inventories.json','utf8', (err, data) => {
-    err?console.info(err):inventoryList = JSON.parse(data)});
+fs.readFile('./data/inventories.json', 'utf8', (err, data) => {
+    err ? console.info(err) : inventoryList = JSON.parse(data)
+});
 
 router.get('/', (req, res) => {
     res.status(200).json(inventoryList);
@@ -14,10 +15,10 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     const itemId = req.params.id;
     const inventoryItem = inventoryList.find(item => item.id === itemId);
-    if(inventoryItem) {
+    if (inventoryItem) {
         res.status(200).json(inventoryItem);
     } else {
-        res.status(400).json({message: 'Cannot find the item in Inventory List'});
+        res.status(400).json({ message: 'Cannot find the item in Inventory List' });
     }
 })
 
@@ -32,29 +33,95 @@ router.put('/:id', (req, res) => {
     inventoryList[i].warehouseName = req.body.warehouseName;
     inventoryList[i].warehouseID = req.body.warehouseID;
 
-    if (req.body.itemName === '' || req.body.description === '' 
-    || req.body.category === '' || req.body.status === '' || req.body.warehouseName === '' ) {
-        res.status(400).json({messages: 'Content cannot be blank, please check your input'});
-        
+    if (req.body.itemName === '' || req.body.description === ''
+        || req.body.category === '' || req.body.status === '' || req.body.warehouseName === '') {
+        res.status(400).json({ messages: 'Content cannot be blank, please check your input' });
+
     } else if (!inventoryList[i]) {
-        res.status(400).json({messages: 'Cannot find the item inventory'});
+        res.status(400).json({ messages: 'Cannot find the item inventory' });
     } else {
         fs.writeFileSync('./data/inventories.json', JSON.stringify(inventoryList));
         //Send the updated item to the user
-        res.status(201).json(inventoryList);   
+        res.status(201).json(inventoryList);
     }
 })
 
 router.delete("/:id", (req, res) => {
-	const selected = inventoryList.find((item) => item.id === req.params.id);
-	const indexValue = inventoryList.indexOf(selected);
+    const selected = inventoryList.find((item) => item.id === req.params.id);
+    const indexValue = inventoryList.indexOf(selected);
 
-	inventoryList.splice(indexValue, 1);
-	res.send("item deleted");
+    inventoryList.splice(indexValue, 1);
+    res.send("item deleted");
+})
+
+router.post('/add', (req, res) => {
+    let itemID = uuid.v4();
+    let emptyCheck = 0;
+    const inventoryList = fs.readFileSync("./data/inventories.json")
+    let inventoryObj = JSON.parse(inventoryList);
+
+    
+    Object.values(req.body).forEach(item => {
+        if (item === "") {
+            return emptyCheck += 1
+        }
+    });
+
+    if (emptyCheck >= 1) {
+        res.status(403).send("Empty Values found");
+    } else if (req.body.quantity <= 0) {
+        res.status(403).send("Quantity cannot be less than or equal to 0")
+    } else {
+        let warehouseList = fs.readFileSync("./data/warehouses.json")
+        let warehouseObj = JSON.parse(warehouseList);
+        let whID = warehouseObj.filter(warehouse => warehouse.name === req.body.name).id
+
+
+        let newItem = {
+            id: itemID,
+            warehouseID: whID,
+            warehouseName: req.body.warehouseName,
+            itemName: req.body.itemName,
+            description: req.body.description,
+            category: req.body.category,
+            status: req.body.status,
+            quantity: req.body.quantity
+        }
+
+        inventoryObj.push(newItem);
+        fs.writeFile("./data/inventories.json", JSON.stringify(inventoryObj, null, 2), (err) => {
+            if (err) {
+                console.log(err)
+            }
+            console.log("new Item added")
+        })
+
+        res.status(201).send(inventoryObj);
+    }
+})
+
 
 module.exports = router;
 
 
+// Ignore -- wrote in advance for my AddForm and AddInventory Pages: 
 
+// Add Warehouse
 
+// let expressURL = "http://localhost:8080"
+// let addInventory = "/inventory/add";
 
+//     let SubmitForm = (e) => {
+//         e.preventDefault();
+//         alert("Item Added");
+//         axios.post(expressURL + addInventory, 
+//             {
+//                 warehouseName: e.target.warehouseName.value,
+//                 itemName: e.target.itemName.value,
+//                 description: e.target.description.value,
+//                 category: e.target.category.value,
+//                 status: e.target.status.value,
+//                 quantity: e.target.quantity.value
+//             })
+//         props.router.history.push("/inventory");
+//     }
